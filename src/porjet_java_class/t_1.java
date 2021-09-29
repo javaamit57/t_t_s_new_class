4487be6daa1705d98bbc3fb40237ddd41d7b359d
package com.demo.controller <javaamit57@gmail.com>
2021-09-29 13:51:54 +0530

    t_1 commit

--- src/porjet_java_class/t_1.java
+++ src/porjet_java_class/t_1.java
@@ -4,5 +4,7 @@ public class t_1 {
     public static void main(String []args){
 
         System.out.println("test------java class");
+        System.out.println("test------java class01");
+        System.out.println("test------java class02");
     }
 }
